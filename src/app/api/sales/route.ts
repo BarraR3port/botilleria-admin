40d1d9758f91a03eef718507f716f51666b9e10c
BarraR3port <barrar3port@gmail.com>
2024-06-11 09:38:09 -0400

    feat: Inicio en la integracion de Ventas

--- src/app/api/sales/route.ts
+++ src/app/api/sales/route.ts
@@ -2,6 +2,59 @@ import { getAuth } from "@/auth/utils";
 import prisma from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
+export async function POST(req: Request) {
+	try {
+		const userId = await getAuth(req);
+		if (!userId)
+			return NextResponse.json({ errors: [{ type: "unauthorized", message: "Sin autorización" }], status: 401 });
+
+		const body = await req.json();
+
+		const { total, products } = body;
+
+		if (!products || !products.length)
+			return NextResponse.json(
+				{ errors: [{ type: "products", message: "Productos requeridos" }] },
+				{ status: 400 }
+			);
+
+		if (!total)
+			return NextResponse.json({ errors: [{ type: "total", message: "Total requerido" }] }, { status: 400 });
+
+		const sale = await prisma.sale.create({
+			data: {
+				total,
+				user: {
+					connect: {
+						id: userId
+					}
+				},
+				products: {
+					create: products.map((product: any) => ({
+						product: {
+							connect: {
+								id: product.id
+							}
+						},
+						quantity: product.quantity,
+						price: product.price,
+						finalPrice: product.finalPrice,
+						appliedDiscount: product.appliedDiscount
+					}))
+				}
+			}
+		});
+
+		return NextResponse.json(sale);
+	} catch (error) {
+		console.log("[PRODUCTS][POST]", error);
+		return NextResponse.json({
+			errors: [{ type: "internal", message: "Ocurrió un error interno, por favor contactar soporte" }],
+			status: 500
+		});
+	}
+}
+
 export async function GET(req: Request) {
 	try {
 		const auth = await getAuth(req);
