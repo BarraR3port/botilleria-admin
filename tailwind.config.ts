1efe56c95c10c65d6d54ddf7db7fe26fb8e8d26a
BarraR3port <barrar3port@gmail.com>
2024-06-14 06:03:26 -0400

    chore: Corregir variables de éxito en los estilos globales y en la configuración de Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -48,17 +48,6 @@ const config = {
 					foreground: "hsl(var(--card-foreground))"
 				},
 				success: {
-					50: "hsl(var(--success-50))",
-					100: "hsl(var(--success-100))",
-					200: "hsl(var(--success-200))",
-					300: "hsl(var(--success-300))",
-					400: "hsl(var(--success-400))",
-					500: "hsl(var(--success-500))",
-					600: "hsl(var(--success-600))",
-					700: "hsl(var(--success-700))",
-					800: "hsl(var(--success-800))",
-					900: "hsl(var(--success-900))",
-					950: "hsl(var(--success-950))",
 					DEFAULT: "hsl(var(--success))",
 					foreground: "hsl(var(--success-foreground))"
 				},
